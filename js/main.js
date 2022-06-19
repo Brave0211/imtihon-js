02dde861893e17d164d6e3a0d9389798c9309c8a
Brave0211 <abduqodirovjasurbek11@gmail.com>
2022-06-19 17:00:10 +0500

    Add js

---
+++ js/main.js
@@ -0,0 +1,68 @@
+// 1-MASALA
+
+// var numbers = [1, 3, 0, 5, 0, 7, 0];
+
+// function func(array) {
+// 	var newArray = [];
+
+// 	for (num of array) {
+// 		if (num == 0) {
+// 			newArray.push(num);
+// 		} else if(num != 0) {
+// 			newArray.unshift(num);
+// 		}
+// 	}
+// 	return newArray.join('');
+// }
+
+// console.log(func(numbers));
+
+
+// 2-MASALA
+
+// var array = [false, true, false, true, true];
+
+// function func(arr) {
+// 	var count = 0;
+// 	for (el of arr) {
+// 		if (el == true) {
+// 			count += 1;
+// 		}
+// 	}
+//   return count;
+// }
+
+// console.log(func(array));
+
+// 3-MASALA
+
+   // var numbers = [1,2,0,-1,-2,3,4]
+
+   // function addition(array){
+   //    var count = 0
+
+   //    for (num of array){
+   //       if(num > 0){
+   //          count = count + num;
+   //       }
+   //    }
+
+   //    return count;
+   // }
+
+   // console.log(addition(numbers));
+
+
+// 4-MASALA
+
+var user = [
+   {name : "Ali", price: 200,},
+   {name : "Umid", price: 800,},
+   {name : "Sardor", price: 400,},
+]
+
+function maxPrice(array){
+   
+}
+
+console.log(maxPrice(userName));
